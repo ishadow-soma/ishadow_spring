2a9bc9a30af7779c1fae52bf6f5cd32e7be09692
ahnseongeun <ast3138@naver.com>
2021-08-04 02:30:09 +0900

    refactor: swagger path update

--- src/main/java/com/soma/ishadow/configures/SwaggerConfig.java
+++ src/main/java/com/soma/ishadow/configures/SwaggerConfig.java
@@ -9,11 +9,13 @@ import springfox.documentation.schema.ModelRef;
 import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Configuration
+@EnableSwagger2
 public class SwaggerConfig {
 
     @Bean
