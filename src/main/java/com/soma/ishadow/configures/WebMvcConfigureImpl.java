61f2a6c36406a6e3aa4c1c1543ec367ba7b7cdb9
ahnseongeun <ast3138@naver.com>
2021-10-11 17:20:02 +0900

    patch test

--- src/main/java/com/soma/ishadow/configures/WebMvcConfigureImpl.java
+++ src/main/java/com/soma/ishadow/configures/WebMvcConfigureImpl.java
@@ -1,5 +1,6 @@
 package com.soma.ishadow.configures;
 
+import com.amazonaws.HttpMethod;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -9,6 +10,12 @@ public class WebMvcConfigureImpl implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") //모든 요청에 대해서
+                .allowedMethods(
+                        HttpMethod.GET.name(),
+                        HttpMethod.HEAD.name(),
+                        HttpMethod.POST.name(),
+                        HttpMethod.PUT.name(),
+                        HttpMethod.DELETE.name())
                 .allowedOrigins("*");
     }
 }
