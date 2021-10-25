21a39e98bba6868a1b57f9fe719296720d420828
ahnseongeun <ast3138@naver.com>
2021-10-25 21:00:46 +0900

    refactor: video page process update

--- src/main/java/com/soma/ishadow/repository/video/VideoRepositoryImpl.java
+++ src/main/java/com/soma/ishadow/repository/video/VideoRepositoryImpl.java
@@ -87,8 +87,8 @@ public class VideoRepositoryImpl extends QuerydslRepositorySupport implements Vi
         QVideo video = QVideo.video;
         QCategoryVideo categoryVideo = QCategoryVideo.categoryVideo;
         QueryResults<Video> result = queryFactory.selectFrom(video)
-                .innerJoin(categoryVideo).on(categoryVideo.category.categoryId.eq(categoryId))
-                .where(video.status.eq(Status.YES), video.videoLevel.between(levelStart, levelEnd))
+                .innerJoin(categoryVideo).on(video.videoId.eq(categoryVideo.categoryVideoId.videoId))
+                .where(categoryVideo.category.categoryId.eq(categoryId),video.status.eq(Status.YES), video.videoLevel.between(levelStart, levelEnd))
                 .orderBy(video.videoId.desc())
                 .offset(pageable.getOffset())
                 .limit(pageable.getPageSize())
