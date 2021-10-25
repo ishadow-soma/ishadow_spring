11eb8e616984d43456eddeb59a05f786e14ee58e
ahnseongeun <ast3138@naver.com>
2021-10-25 22:28:59 +0900

    refactor: constatn video server url update

--- src/main/java/com/soma/ishadow/configures/Constant.java
+++ src/main/java/com/soma/ishadow/configures/Constant.java
@@ -10,8 +10,8 @@ public class Constant {
     public static Long bookmarkCount = 0L;
     public static final Long DEFAULT_SIZE = 10L;
     public static final String startFilePath = "scp -i /home/ubuntu/key_fair/ishadow_key.pem -r ";
-    public static final String endFilePath = " root@media.ishadow.kr:/home/ubuntu/";
-    public static final String mkdirPath = "ssh -i \"/home/ubuntu/key_fair/ishadow_key.pem\" root@media.ishadow.kr";
+    public static final String endFilePath = " ubuntu@media.ishadow.kr:/home/ubuntu/";
+    public static final String mkdirPath = "ssh -i \"/home/ubuntu/key_fair/ishadow_key.pem\" ubuntu@media.ishadow.kr";
     public static final String videoBasePath = "https://media.ishadow.kr/video/";
     public static final String imageBasePath = "https://media.ishadow.kr/image/";
     public static final String IMAGE_PNG_FORMAT = "png";
